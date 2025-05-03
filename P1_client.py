cd71dc5cfbcfe739843e76cc6e515e4a2bb883ca
Extra <134788809+lyf29@users.noreply.github.com>
2025-05-03 13:15:39 +0530

    Delete P1_client.py

--- P1_client.py
+++
@@ -1,9 +0,0 @@
-import xmlrpc.client
-
-server = xmlrpc.client.ServerProxy("http://localhost:8000")
-
-number = int(input("Enter an integer to compute its factorial: "))
-
-result = server.compute_factorial(number)
-
-print(f"The factorial of {number} is {result}")
