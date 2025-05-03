ac87e086588e5525f9c64a6a2b591e26488ae5d5
Extra <134788809+lyf29@users.noreply.github.com>
2025-05-03 13:15:22 +0530

    Delete HotelInterface.java

--- HotelInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.Map;
-
-public interface HotelInterface extends Remote {
-    String bookRoom(String guestName) throws RemoteException;
-    String cancelBooking(String guestName) throws RemoteException;
-    Map<String, String> getAllBookings() throws RemoteException;
-}
